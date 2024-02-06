5d270ca04f7b7301f412b08161ee21ef9c05784d
salmonindaeyo <iamfhaa@hotmail.com>
2024-02-06 17:01:15 +0700

    Update server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const socketServer = require("./socketServer");
+const authRoutes = require("./routes/authRoutes");
+const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
@@ -12,4 +14,27 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// register the routes
+app.use("/api/auth", authRoutes);
+app.use("/api/friend-invitation", friendInvitationRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Running leaw");
+});
+
+const server = http.createServer(app);
 socketServer.registerSocketServer(server);
+
+console.log("start server");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("database connection failed. server not start");
+    console.log(err);
+  });
