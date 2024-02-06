e1897d89a027518ba319dfdaf176ba40c1623edd
salmonindaeyo <iamfhaa@hotmail.com>
2024-02-06 17:28:21 +0700

    Update server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// register the routes
+// register the routes.
 app.use("/api/auth", authRoutes);
 app.use("/api/friend-invitation", friendInvitationRoutes);
 
